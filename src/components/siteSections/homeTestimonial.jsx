9d7a5890d95ec226fc1b3e42c9b1be6723756fec
dinethprasanna <dinethprasanna58@gmail.com>
2025-07-22 16:28:54 +0530

    changes home section order

--- src/components/siteSections/homeTestimonial.jsx
+++ src/components/siteSections/homeTestimonial.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-import { Autoplay, Navigation } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
 const testimonials = [
     {
