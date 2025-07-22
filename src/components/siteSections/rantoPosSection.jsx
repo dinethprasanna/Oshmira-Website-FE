cf2377553287adacf5665e46cac69274cbffad5d
dinethprasanna <dinethprasanna58@gmail.com>
2025-07-22 17:23:59 +0530

    Home Page UI dev V1.1 Done 22-7-2025

--- src/components/siteSections/rantoPosSection.jsx
+++ src/components/siteSections/rantoPosSection.jsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 function RantoPosSection() {
     return (
         <section className="ranto-pos-section relative w-full max-w-(--max-width) mx-auto px-3 md:px-6 lg:px-4 py-8 md:py-16 lg:py-24">
-            <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-9 lg:gap-12 py-8 md:py-9 lg:py-12 px-8 md:px-9 lg:px-12 rounded-md bg-(--bg-second-col)">
+            <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-9 lg:gap-12 py-8 md:py-9 lg:py-12 px-8 md:px-9 lg:px-12 rounded-md bg-(--bg-second-col) shadow-[-1px_1px_2px_rgba(255,255,255,0.4)_inset]">
                 <div className="ranto-pos-info col-span-1 md:col-span-2 flex flex-col justify-center">
                     <h2 className="text-white text-center md:text-left font-inter-semibold text-2xl md:text-3xl lg:text-4xl mb-4 lg:mb-5"><span className='gradient-text-anime'>Ranto POS</span></h2>
                     <p className="font-inter-light text-white text-center md:text-left text-base md:text-lg max-w-[496px] mb-6 mx-auto md:mx-0">
--- src/components/siteSections/singleCard.jsx
+++ src/components/siteSections/singleCard.jsx
@@ -1,6 +1,6 @@
 const SingleCard = ({ image, title, description }) => {
   return (
-    <div className="bg-(--bg-second-col) rounded-3xl p-4 lg:px-8 lg:py-16 min-h-[240px] lg:h-[324px] text-center w-full h-full block singleCard shine-card-overlay">
+    <div className="bg-(--bg-second-col) rounded-3xl p-4 lg:px-8 lg:py-16 min-h-[240px] lg:h-[324px] text-center w-full h-full block singleCard shine-card-overlay shadow-[-1px_1px_2px_rgba(255,255,255,0.4)_inset]">
       <img src={image} alt={title} className="mb-4 w-[58px] h-auto object-contain" />
       <h3 className="text-white text-base font-inter-semibold text-left mb-[18px]">{title}</h3>
       <p className="text-white text-left text-sm font-inter-thin">{description}</p>
