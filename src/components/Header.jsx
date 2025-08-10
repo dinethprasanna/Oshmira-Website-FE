2bb86c2cfe548fb1411eba9b7b7db135d15ae9aa
dinethprasanna <dinethprasanna58@gmail.com>
2025-08-10 14:12:44 +0530

    Mobile header issue fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Link, NavLink } from "react-router";
 import { useState, useRef, useEffect } from 'react';
 import gsap from "gsap";
@@ -6,7 +6,6 @@ import { useGSAP } from "@gsap/react";
 
 function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
     const headerContainerRef = useRef();
     const [isScrolled, setIsScrolled] = useState(false);
 
@@ -55,22 +54,23 @@ function Header() {
         });
     }, { scope: headerContainerRef });
 
+    // Function to close mobile menu
+    const closeMobileMenu = () => {
+        setIsMobileMenuOpen(false);
+    };
 
     return (
         <header ref={headerContainerRef} className="site-header mx-auto w-full max-w-full px-3 md:px-6 lg:px-4 fixed top-[16px] md:top-[24px] lg:top-[36px] text-white z-99">
             <div className="max-w-(--max-width) mx-auto w-full px-[18px] py-3 bg-(--bg-second-col) rounded-md relative">
                 {/* desktop nav */}
                 <div className="desktop-nav hidden lg:flex justify-between items-center gap-3">
-                    <div className="">
-
+                    <div>
                         <Link to="/" className="header-logo relative inline-block group w-fit">
-                            {/* Default image */}
                             <img
                                 src="/header/Oshmira-logo.png"
                                 alt="Oshmira"
                                 className="w-[100px] opacity-100 group-hover:opacity-0 transition-all"
                             />
-                            {/* Hover image */}
                             <img
                                 src="/header/Oshmira-logo-white.png"
                                 alt="Oshmira Hover"
@@ -80,59 +80,51 @@ function Header() {
                     </div>
                     <div className="desk-nav-links flex justify-center gap-6">
                         <NavLink to={'/'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
                         }>
                             Home
                         </NavLink>
                         <NavLink to={'/about-us'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
-                        } >
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
+                        }>
                             About us
                         </NavLink>
                         <NavLink to={'/solutions'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
-                        } >
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
+                        }>
                             Solutions
                         </NavLink>
                         <NavLink to={'/services'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
-                        } >
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
+                        }>
                             Services
                         </NavLink>
                         <NavLink to={'/portfolio'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
-                        } >
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
+                        }>
                             Portfolio
                         </NavLink>
                         <NavLink to={'/packages'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
-                        } >
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
+                        }>
                             Packages
                         </NavLink>
                         <NavLink to={'/contact-us'} className={({ isActive }) =>
-                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                            }`
-                        } >
+                            `m-0 p-0 font-inter-semibold text-sm transition-colors hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"}`
+                        }>
                             Contact us
                         </NavLink>
                     </div>
                     <div className="nav-btn1">
                         <Link to={'/contact-us'}>
-                            <Button >Get started</Button>
+                            <Button>Get started</Button>
                         </Link>
                     </div>
                 </div>
 
-                {/* Mobile nav with hamburge menu */}
+                {/* Mobile nav with hamburger menu */}
                 <div className="mobile-nav flex justify-between items-center lg:hidden">
                     <Link to="/" className="relative inline-block group">
-                        {/* Default logo */}
                         <img
                             src="/header/Oshmira-logo.png"
                             alt="Oshmira"
@@ -156,9 +148,6 @@ function Header() {
                     </button>
                 </div>
 
-
-
-
                 <div className="nav-animate-border rounded-md"></div>
             </div>
             {/* Mobile menu links dropdown */}
@@ -172,25 +161,79 @@ function Header() {
                 }}
             >
                 <div className="p-6 border-b-2 border-white flex flex-col gap-4">
-                    <NavLink to={'/'} className={({ isActive }) =>
-                        `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "text-orange-500" : "text-white"
-                        }`
-                    } >
+                    <NavLink
+                        to={'/'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        to={'/about-us'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        About us
+                    </NavLink>
+                    <NavLink
+                        to={'/solutions'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Solutions
+                    </NavLink>
+                    <NavLink
+                        to={'/services'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Services
+                    </NavLink>
+                    <NavLink
+                        to={'/portfolio'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Portfolio
+                    </NavLink>
+                    <NavLink
+                        to={'/packages'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Packages
+                    </NavLink>
+                    <NavLink
+                        to={'/contact-us'}
+                        className={({ isActive }) =>
+                            `block text-sm font-semibold text-white hover:text-orange-500 ${isActive ? "!text-orange-500" : "text-white"}`
+                        }
+                        onClick={closeMobileMenu}
+                    >
                         Contact us
                     </NavLink>
-                    <Link to="/" className="block text-sm font-semibold text-white hover:text-orange-500">Home</Link>
-                    <Link to="/about-us" className="block text-sm font-semibold text-white hover:text-orange-500">About us</Link>
-                    <Link to="/solutions" className="block text-sm font-semibold text-white hover:text-orange-500">Solutions</Link>
-                    <Link to="/services" className="block text-sm font-semibold text-white hover:text-orange-500">Services</Link>
-                    <Link to="/portfolio" className="block text-sm font-semibold text-white hover:text-orange-500">Portfolio</Link>
-                    <Link to="/packages" className="block text-sm font-semibold text-white hover:text-orange-500">Packages</Link>
-                    <Link to="/contact-us" className="block text-sm font-semibold text-white hover:text-orange-500">Contact us</Link>
-                    <Link to="/contact-us">
+                    <Link
+                        to="/contact-us"
+                        className="block text-sm font-semibold text-white hover:text-orange-500"
+                        onClick={closeMobileMenu}
+                    >
                         <Button className="mt-2">Get started</Button>
                     </Link>
                 </div>
             </div>
-        </header >
+        </header>
     );
 }
 
